3d359a4c7a7b7a5f15a3ab7247233bfbd6593dc3
hasira09 <hasiramahel09@gmail.com>
2023-06-04 02:02:30 +0530

    Module 02 - Done
    CRUD Operations

---
+++ Module 03/Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace Module_03.Models;
+
+public class Pizza
+{
+    public int Id { get ; set ; }
+    public string? Name { get ; set ; }
+    public bool IsGlutenFree { get ; set ; }
+}
---
+++ Module 03/Services/PizzaService.cs
@@ -0,0 +1,45 @@
+using Module_03.Models;
+
+namespace Module_03.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get ; }
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Cheese and Chillie", IsGlutenFree = false},
+            new Pizza { Id = 2, Name = "Spicy Mushroom", IsGlutenFree = true}  
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get (int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
+
+            Pizzas[index] = pizza;
+    }
+}
